20b1a12d458cb23e80d1c9f39670b70ab0e46033
ShinMini <gusals121234@gmail.com>
2023-02-21 09:59:16 +0900

    assets: set up main index file, to import alias

---
+++ assets/svg/mini_card/index.ts
@@ -0,0 +1,5 @@
+import MiniCardBlack from './Mini_Card_Black.svg'
+import MiniCardPurPle from './Mini_Card_PurPle.svg'
+import MiniCardWhite from './Mini_Card_White.svg'
+
+export {MiniCardBlack, MiniCardWhite, MiniCardPurPle}
---
+++ assets/svg/modu_card/index.ts
@@ -0,0 +1,6 @@
+import BlueBackgroundMobyCard from './blue-background-moby-card.svg';
+import GridMobyCard from './grid-moby-card.svg';
+import RidingMobyCard from './riding-moby-card.svg';
+import WhiteBackgroundMobyCard from './white-background-moby-card.svg';
+
+export {BlueBackgroundMobyCard, GridMobyCard, RidingMobyCard, WhiteBackgroundMobyCard}
