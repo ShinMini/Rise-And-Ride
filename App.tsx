f5b845e9d4773fb8f7fee9ca2e048b21aea6817a
ShinMini <77220824+ShinMini@users.noreply.github.com>
2023-03-06 15:52:47 +0900

    update: for redux store update

--- App.tsx
+++ App.tsx
@@ -1,28 +1,37 @@
 import React, {useCallback} from 'react'
 import {Provider} from 'react-redux';
-import {store} from 'stores';
+import {store} from 'src/stores/slice';
+
+import Main from "src/Main";
+import {customFonts} from "src/hooks/useLoadAssets";
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 import {useFonts} from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
-import Main from "src/Main";
-import {customFonts} from "src/hooks/useLoadAssets";
+import AppLoading from 'src/screens/Loading/AppLoading';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
     const [fontsLoaded] = useFonts(customFonts);
 
     const onLayoutRootView = useCallback(async () => {
+
+
         if (fontsLoaded) {
             await SplashScreen.hideAsync();
         }
     }, [fontsLoaded]);
 
     if (!fontsLoaded) {
-        return null;
+        return <AppLoading />;
     }
 
-  return (
-    <Provider store={store}>
-        <Main />
-    </Provider>
-  );
+    return (
+        <SafeAreaView onLayout={onLayoutRootView} style={{flex: 1}}>
+            <Provider store={store}>
+                <Main />
+            </Provider>
+        </SafeAreaView>
+    );
 };
