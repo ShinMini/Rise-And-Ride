eef7fe01f74c387f81dd7c456019e7498f5b1f18
ShinMini <77220824+ShinMini@users.noreply.github.com>
2023-03-09 11:35:50 +0900

    update: change the redux call func

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,11 +1,8 @@
-import {useSelector} from "react-redux";
-import {ColorSchemeState} from "stores/colorScheme";
 import React, {FC} from "react";
 import {ThemeProvider} from "styled-components/native";
 import AppRouter from "../AppRouter";
 import {StatusBar} from "expo-status-bar";
 import useThemes from "./hooks/useTheme";
-import { RootState } from "./stores/slice";
 
 // 1. 로그인 유저 판별
 // 2. 로그인 유저가 아니면 로그인 페이지로 이동
